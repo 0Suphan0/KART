7ab320341b97cc0ac6350563f774e6a23ff25ff4
0Suphan0 <80632509+0Suphan0@users.noreply.github.com>
2023-01-18 01:05:17 +0300

    The End.

--- myCardProject/src/app/app.component.ts
+++ myCardProject/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myCardProject';
+  posts=[
+    {title:"Doğa Yürüyüşü",imageUrl:"assets/tree.jpeg",userName:"@cbdoga",content:"Hadi biraz maçka parkında turlayalım"},
+    {title:"Tırmanış",imageUrl:"assets/mountain.jpeg",userName:"@cbuludag",content:"Hızlı bir tırmanış etkinliği"},
+    {title:"Dağ bisikleti",imageUrl:"assets/biking.jpeg",userName:"@cbbisiklet",content:"Yarışlar hız ve huzur."}
+  ]
 }
